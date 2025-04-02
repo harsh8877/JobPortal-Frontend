27691f007966a07983081e6a8b54df685f03d976
Harsh Vasoya <vasoyaharsh123@gmail.com>
2025-04-02 13:14:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Jobs from "./Pages/Jobs";
